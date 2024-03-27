110525e14e640fcd7459f7b34f2d0553380c31d5
Young Ayodele <young.ayodele@gmail.com>
2024-03-27 18:20:08 +0100

    credentials file

--- auth.py
+++
@@ -1,9 +0,0 @@
-
-
-headers = {
-    "accept": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NjExNTJkMDczMTY5NjFlZjdmNmRlMzc1YTFmOWViNCIsInN1YiI6IjY2MDQyMjAzMDQ3MzNmMDE2NGVjNDk3NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.eum1HWKm2kbYwtrQ7aUYb9AgldQPIUGdiE9Yy-qxNbk"
-}
-
-
-
